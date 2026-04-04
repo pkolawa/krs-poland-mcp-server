222f5e5cc2d0828af851aac91e9385706193c87d
Piotr Kolawa <p.kolawa@gmail.com>
2026-04-04 18:52:04 +0200

    getting rid of dead code

--- src/tools/index.ts
+++
@@ -1,7 +0,0 @@
-import { getKrsCurrentExtractTool } from "./currentExtract.js";
-import { getKrsFullExtractTool } from "./fullExtract.js";
-
-export const tools = {
-  "get-krs-current-extract": getKrsCurrentExtractTool,
-  "get-krs-full-extract": getKrsFullExtractTool,
-};
